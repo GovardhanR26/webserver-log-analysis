370c40af53a24c5da8012c47c6b492e9507476a8
Govardhan R <76608879+GovardhanR26@users.noreply.github.com>
2022-12-19 23:33:03 +0530

    page analysis - first commit

---
+++ log_page/MapperPage.java
@@ -0,0 +1,25 @@
+package log_page;
+
+//Importing libraries
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class MapperPage extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+	// Map function
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter rep)
+			throws IOException {
+
+		String line = value.toString();
+		String[] fields = line.split(" ");
+		if (fields.length == 10) {
+			output.collect(new Text(fields[6]), new IntWritable(1));
+		}
+	}
+}
