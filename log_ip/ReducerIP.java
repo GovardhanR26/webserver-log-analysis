1b8d282407b903abf3b6de0b014c88a3fc364560
Govardhan R <76608879+GovardhanR26@users.noreply.github.com>
2022-12-19 23:36:19 +0530

    IP analysis - all files

---
+++ log_ip/ReducerIP.java
@@ -0,0 +1,45 @@
+package log_ip;
+
+//Importing libraries
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class ReducerIP extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+//	static int keyCount = 0;
+//	static Text maxIP = null;
+//	static int maxVal = 0;
+
+	// Reduce function
+	public void reduce(Text key, Iterator<IntWritable> value, OutputCollector<Text, IntWritable> output, Reporter rep)
+			throws IOException {
+
+		int count = 0;
+
+		// Counting the frequency of each words
+		while (value.hasNext()) {
+			IntWritable i = value.next();
+			count += i.get();
+		}
+
+//		if (count > maxVal) {
+//			maxVal = count;
+//			maxIP = key;
+//		}
+		
+		output.collect(key, new IntWritable(count));
+
+//		keyCount++;
+//
+//		if (keyCount == 333919) {
+//			output.collect(maxIP, new IntWritable(maxVal)); // correct code
+//		}
+
+//		output.collect(key, new IntWritable(keyCount)); // code to count no. of keys
+	}
+}
