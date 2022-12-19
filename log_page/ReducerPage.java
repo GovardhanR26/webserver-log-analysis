fb915d3cb54e9550f990746a14a45fec9d2d7766
Govardhan R <76608879+GovardhanR26@users.noreply.github.com>
2022-12-19 23:58:25 +0530

    comments removed

--- log_page/ReducerPage.java
+++ log_page/ReducerPage.java
@@ -11,9 +11,6 @@ import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 
 public class ReducerPage extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-//	static int keyCount = 0;
-//	static Text maxPage = null;
-//	static int maxVal = 0;
 
 	// Reduce function
 	public void reduce(Text key, Iterator<IntWritable> value, OutputCollector<Text, IntWritable> output, Reporter rep)
@@ -26,20 +23,7 @@ public class ReducerPage extends MapReduceBase implements Reducer<Text, IntWrita
 			IntWritable i = value.next();
 			count += i.get();
 		}
-		
-//		if(count>maxVal) {
-//			maxVal = count;
-//			maxPage = key;
-//		}
 
-//		keyCount++;
-		
 		output.collect(key, new IntWritable(count));
-
-//		if (keyCount == 42376) {
-//			output.collect(maxPage, new IntWritable(maxVal)); // correct code
-//		}
-
-//		output.collect(key, new IntWritable(keyCount)); // to count no. of keys
 	}
 }
